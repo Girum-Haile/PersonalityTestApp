dc97eec3593fc8bb0fd0af17271d97fa0448bb2c
Girum-Haile <35455380+Girum-Haile@users.noreply.github.com>
2020-02-06 22:47:20 +0300

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
-
-
 }
-class MyApp extends StatelessWidget {
- Widget build(BuildContext context){
-   return MaterialApp(home: Scaffold(
-     appBar:AppBar(title:Text('my first app'),),
-     body:Text('my first app'),
-     ),);
 
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('my first app'),
+          ),
+          body: Column(
+            children: [
+              Text("The questions!"),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: null,
+              ),
+            ],
+          )),
+    );
   }
 }
