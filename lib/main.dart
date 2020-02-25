d85d1b0107db1ce10ca7354466eda7c59a03a7d8
Girum-Haile <35455380+Girum-Haile@users.noreply.github.com>
2020-02-25 14:43:46 +0300

    styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -40,18 +41,9 @@ class _MyAppState extends State<MyApp> {
           body: Column(
             children: [
               Question(question[_questionIndex]),
-              RaisedButton(
-                child: Text('answer 1'),
-                onPressed: _answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('Answer 2'),
-                onPressed: _answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('Answer 3'),
-                onPressed: _answerQuestion,
-              ),
+              Answer(_answerQuestion),
+              Answer(_answerQuestion),
+              Answer(_answerQuestion),
             ],
           )),
     );
